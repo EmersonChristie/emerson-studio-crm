96a7b40f14d560ace21423a144faab1f49bafcf3
Emerson <emerson.art.studios@gmail.com>
2024-07-14 17:05:05 -0700

    Move backend code to backend directory

--- backend/src/api/contact/contactRepository.ts
+++ backend/src/api/contact/contactRepository.ts
@@ -1,6 +1,5 @@
 import { Contact } from '@/api/contact/contactModel';
 import prisma from '../../../prisma/client';
-
 export const contacts: Contact[] = await prisma.contact.findMany();
 
 export const contactRepository = {
